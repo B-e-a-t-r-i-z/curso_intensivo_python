86626f67d7959fb6c30f60ba1b21a5857cd86f3b
B-e-a-t-r-i-z <beatriz.almeida2001@outlook.com>
2023-09-30 14:41:26 -0300

    novos exerícios do chapter 10

--- 10_chapter.py
+++ 10_chapter.py
@@ -1,5 +1,5 @@
 # EXERCÍCIO 10.1
-
+'''
 filename = "learning_python.txt"
 
 with open(filename) as file_object:
@@ -23,3 +23,71 @@ for line in lines:
 print("######################################################################")
 
 #EXERCÍCIO 10.3
+filename = 'guest.txt'
+nome = input("Qual é o seu nome? ")
+
+with open(filename, 'a') as file_object:
+    file_object.write(nome)
+
+print("######################################################################")
+
+#EXERCÍCIO 10.4
+
+filename = "guest_book.txt"
+resposta = True
+while resposta:
+    pergunta1 = input("\nQual é o seu nome de usuário? ")
+    print("\nOlá! Seja bem-vindo(a)!!! " + pergunta1)
+    pergunta2 = input("\nDeseja inserir um novo usuário?(sim ou não) ")
+    
+    with open(filename, "a") as file_object:
+        file_object.write(pergunta1 + "\n")
+        
+    if pergunta2 == "não":
+        resposta = False
+
+print("######################################################################")
+
+#EXERCÍCIO 10.5
+
+filename = "pqamamosprogramacao.txt"
+
+while True:
+    pergunta_1 = input("\nPor que você amar programar? ")
+    pergunta_2 = input("\nDeseja perguntar a outra pessoa? (s ou n) ")
+    
+    with open(filename, "a") as file_object:
+        file_object.write(pergunta_1 + "\n")
+        
+    if pergunta_2 == "n":
+        break
+
+print("######################################################################")
+
+#EXERCÍCIO 10.6
+
+try:
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Digite o segundo número: "))
+
+    resultado = num1 + num2
+
+    print("A soma é igual a " + str(resultado))
+
+except ValueError:
+    print("Opa! Parece que você inseriu algum número válido. Tente novamente!")
+
+print("######################################################################")
+
+#EXERCÍCIO 10.7
+'''
+
+    
+    
+    
+
+
+
+
+
+
--- README.md
+++
@@ -1,5 +0,0 @@
-## Resoluções das questões do 🕮 Curso Intensivo de Python de Eric Matthes
-
-Status: Em desenvolvimento 💻
-
-- Atualizado até o capitulo 10
